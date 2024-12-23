fb668e68142b443a669aa78321c122b750d0ce34
Cyril Coutelier <cyril@flat.io>
2024-12-23 19:41:15 +0100

    refactor: prepare day-02 for part 2

--- day-02-1-Red-Nosed_Reports/src/lib/solver.rs
+++ day-02-1-Red-Nosed_Reports/src/lib/solver.rs
@@ -1,6 +1,37 @@
 const MAX_DIFF: i32 = 3;
 const MIN_DIFF: i32 = 1;
 
+fn is_list_safe<F: Fn(i32, i32) -> bool>(list: &[i32], is_adjacent_acceptable: &F) -> bool {
+    list.windows(2).all(|w| is_adjacent_acceptable(w[0], w[1]))
+}
+
+fn is_adjacent_diff_acceptable(left: i32, right: i32) -> bool {
+    let diff = (left - right).abs();
+    (MIN_DIFF..=MAX_DIFF).contains(&diff)
+}
+
+fn is_greater(left: i32, right: i32) -> bool {
+    left > right
+}
+
+fn is_lower(left: i32, right: i32) -> bool {
+    left < right
+}
+
+struct GreaterLower {
+    nb_greater: usize,
+    nb_lower: usize,
+}
+
+impl GreaterLower {
+    fn new() -> Self {
+        Self {
+            nb_greater: 0,
+            nb_lower: 0,
+        }
+    }
+}
+
 struct Report {
     list: Vec<i32>,
 }
@@ -11,22 +42,39 @@ impl Report {
     }
 
     pub fn is_safe(&self) -> bool {
-        self.are_valid_diff() && (self.is_increasing() || self.is_decreasing())
+        let comparator = self.get_comparator();
+        match comparator {
+            Some(comparator) => self.test_adjacent(|left, right| {
+                comparator(left, right) && is_adjacent_diff_acceptable(left, right)
+            }),
+            None => false,
+        }
     }
 
-    fn are_valid_diff(&self) -> bool {
-        self.list.windows(2).all(|w| {
-            let diff = (w[0] - w[1]).abs();
-            (MIN_DIFF..=MAX_DIFF).contains(&diff)
-        })
+    fn get_comparator(&self) -> Option<fn(i32, i32) -> bool> {
+        let greater_lower = self.get_nb_greater_lower();
+        match greater_lower.nb_greater.cmp(&greater_lower.nb_lower) {
+            std::cmp::Ordering::Greater => Some(is_greater),
+            std::cmp::Ordering::Less => Some(is_lower),
+            std::cmp::Ordering::Equal => None,
+        }
     }
 
-    fn is_increasing(&self) -> bool {
-        self.list.windows(2).all(|w| w[0] < w[1])
+    fn get_nb_greater_lower(&self) -> GreaterLower {
+        self.list
+            .windows(2)
+            .fold(GreaterLower::new(), |mut acc, w| {
+                match w[0].cmp(&w[1]) {
+                    std::cmp::Ordering::Less => acc.nb_lower += 1,
+                    std::cmp::Ordering::Greater => acc.nb_greater += 1,
+                    std::cmp::Ordering::Equal => (),
+                }
+                acc
+            })
     }
 
-    fn is_decreasing(&self) -> bool {
-        self.list.windows(2).all(|w| w[0] > w[1])
+    fn test_adjacent<F: Fn(i32, i32) -> bool>(&self, callback: F) -> bool {
+        is_list_safe(&self.list, &callback)
     }
 }
 
