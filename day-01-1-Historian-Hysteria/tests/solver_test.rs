622facaa060747914f7db15fab824c4270995b0d
Cyril Coutelier <cyril@flat.io>
2024-12-23 13:31:53 +0100

    feat: day-01-part-1

---
+++ day-01-1-Historian-Hysteria/tests/solver_test.rs
@@ -0,0 +1,39 @@
+mod extract_numbers {
+    use aoc2024::solver::extract_numbers;
+
+    #[test]
+    fn only_digits_3_4() {
+        // When
+        let input = "3   4";
+        // When
+        let result = extract_numbers(input);
+        // Then
+        assert_eq!(result, (3, 4));
+    }
+
+    #[test]
+    fn numbers_85540_67702() {
+        // When
+        let input = "85540   67702";
+        // When
+        let result = extract_numbers(input);
+        // Then
+        assert_eq!(result, (85540, 67702));
+    }
+}
+
+mod process_lines {
+    use aoc2024::solver::process_lines;
+
+    #[test]
+    fn puzzle_subject_example() {
+        // When
+        let input = &["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"];
+
+        // When
+        let result = process_lines(input.iter().map(|x| x.to_string()));
+
+        // Then
+        assert_eq!(result, "11");
+    }
+}
