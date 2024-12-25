e13adff06cd0acc0bbfbffca88e63d55e6ee6a28
Cyril Coutelier <cyril@flat.io>
2024-12-25 22:45:00 +0100

    chore: show elapsed time

--- day-02-2-Red-Nosed_Reports/src/main.rs
+++ day-02-2-Red-Nosed_Reports/src/main.rs
@@ -1,9 +1,16 @@
 #![warn(clippy::pedantic)]
 
+use std::time::Instant;
+
 use aoc2024::common::process_lines_from_args_file;
 use aoc2024::solver::process_lines;
 
 fn main() {
+    let now = Instant::now();
+
     let result = process_lines_from_args_file(process_lines);
     println!("The solution is `{result}`");
+
+    let elapsed = now.elapsed();
+    println!("Solved in: {elapsed:.2?}");
 }
